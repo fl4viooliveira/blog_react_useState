f1aafccd17642a3ce163706527196fd1cbe3d3d8
Flavio Oliveira <61277545+fl4viooliveira@users.noreply.github.com>
2021-10-18 11:16:33 +0100

    State Hooke example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import "./styles.css";
+import React, { useState } from "react";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1 className="text">State Hook</h1>
+      <span className="text">{count} </span>
+      <div>
+        <button className="btn-add" onClick={() => setCount(count + 1)}>
+          +1
+        </button>
+        <button className="btn-take" onClick={() => setCount(count - 1)}>
+          -1
+        </button>
+      </div>
+      <button className="btn" onClick={() => setCount(count - count)}>
+        Reset
+      </button>
     </div>
   );
 }
